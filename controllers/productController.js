483cf530168bcbe6a7a0f8410509754e57c7a58e
jesus7110 <juthikahazra11@gmail.com>
2023-10-26 19:19:24 +0530

    productModel

---
+++ controllers/productController.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
