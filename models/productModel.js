021829d0c84e1b4c94b1da48c2cdfea4c4974545
jesus7110 <juthikahazra11@gmail.com>
2023-10-26 19:20:19 +0530

    productModel update and controller for products

---
+++ models/productModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+});
