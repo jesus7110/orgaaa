9406480371a1dfe2813c28c49ae40b0213275200
ayan hazra <juthikahazra11@gmail.com>
2023-10-28 15:10:12 +0530

    Router Updated

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const express = require('express')
 require('dotenv').config();
 const connectDB = require("./config/database");
+const userRouter = require('./routers/userRouter')
 const app = express()
 
 //server start test
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Server running');
 })
 //mongoDB connectiongit
 connectDB();
 
-// Import the userController
-const userController = require('./controllers/userController');
 
-//import productcontroller
 
-// Define routes and associate them with controller functions
-app.post('/users', userController.createUser); // Create a new user
-//app.get('/users', userController.getAllUsers); // Get all users
-app.get('/genrateOTP', userController.generateOTP);
 app.listen(process.env.PORT || 5000)
