68bbd26a616db39df745aa39243f9b52572e4452
unknown <juthikahazra11@gmail.com>
2023-11-06 16:35:39 +0530

    Product addproductcontroller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
-const { Product } = require('../models/productModel');
+const { product } = require('../models/productModel');
 
+/*
 module.exports.addProduct = async (req, res) => {
   const product = await Product.findOne({
     productid: req.body.productid
@@ -10,3 +11,27 @@ module.exports.addProduct = async (req, res) => {
   
   return res.status(200).send("Product Saved successfully!");
 }
+*/
+
+// Function to add a product to the database
+const addProduct = async (req, res) => {
+  try {
+    const newProductData = req.body; // Assuming the product data is in the request body
+
+    const existingProduct = await product.findOne({ productid: newProductData.productid });
+
+    if (existingProduct) {
+      return res.status(400).json({ success: false, message: 'Product with the same productID already exists.' });
+    }
+
+    const createdProduct = await product.create(newProductData);
+
+    return res.status(201).json({ success: true, message: 'Product added successfully', data: createdProduct });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: 'An error occurred while adding the product.', error });
+  }
+};
+
+module.exports = {
+  addProduct,
+};
