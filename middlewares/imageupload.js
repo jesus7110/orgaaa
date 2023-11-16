ce58891d4103a9a5d7c1da56b4efa41b6f153950
unknown <juthikahazra11@gmail.com>
2023-11-16 19:18:23 +0530

    image of product done

--- middlewares/imageupload.js
+++ middlewares/imageupload.js
@@ -12,8 +12,23 @@ var storage = multer.diskStorage({
 })
 
 
-var upload = multer ({
-    storage: storage
+var upload =multer ({
+    storage: storage,
+    fileFilter: function(req, file, callback) {
+        if(
+            file.mimetype == "image/png" ||
+            file.mimetype == "image/jpg" 
+        ){
+            callback(null,true)
+        }else{
+            console.log('only jpg & png file supported!')
+            callback(null, false)
+        }
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 2
+        
+    }
 })
 
 module.exports = upload
