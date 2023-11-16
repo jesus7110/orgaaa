2e0397dfabbcf063be226c6da7c97271adbbb77e
unknown <juthikahazra11@gmail.com>
2023-11-16 18:31:44 +0530

    image

--- middlewares/imageupload.js
+++ middlewares/imageupload.js
@@ -1,1 +1,17 @@
-const 
+const path = require('path')
+const multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function(req, file, cb) {
+        let ext = path.extname(file.originalname)
+        cb(null, Date.now() + ext)
+    }
+})
+
+var upload =multer ({
+    storage: storage,
+    fileFilter: function
+})
