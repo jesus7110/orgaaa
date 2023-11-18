d9bee32d2ad18ab151ed2cf69ad71114930566d2
unknown <juthikahazra11@gmail.com>
2023-11-18 11:00:16 +0530

    admoin username

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,1 +1,12 @@
- const { Schema, model }
+ const { Schema, model } = require('mongoose')
+
+
+ const adminSchema = Schema({
+    usename: {
+        type: String,
+        require: true 
+    },
+    password: {
+        
+    }
+ })
