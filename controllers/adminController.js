e5d7d4c71a31a8295a41bf5f10557b60fbd36a87
unknown <juthikahazra11@gmail.com>
2023-11-18 14:09:17 +0530

    admincontroller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,3 +3,14 @@ const _ = require("lodash")
 const axios = require("axios")
 
 
+const { Admin } = require('../models/adminModel');
+
+
+
+module.exports.admin = async (req, res) => {
+    const admin = await Admin.findOne({
+        username: req.body.username
+    });
+
+    
+}
