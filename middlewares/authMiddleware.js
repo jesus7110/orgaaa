3346af429b2641b051fc8ff051b53991c41768ed
unknown <juthikahazra11@gmail.com>
2023-11-20 17:40:30 +0530

    auth middleware authenticationToken Fucntion

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,30 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-const authenticationToken = (req, res, next) => {
-    
-}
+const authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Unauthorized: Token not provided' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ success: false, message: 'Forbidden: Invalid token' });
+    }
+
+    req.user = user; // Set the user information in the request object
+    next();
+  });
+};
+
+const authorizeAdmin = (req, res, next) => {
+  const user = req.user;
+
+  if (!user || user.role !== 'admin') {
+    return res.status(403).json({ success: false, message: 'Forbidden: Access denied' });
+  }
+
+  next();
+};
+
+module.exports = { authenticateToken, authorizeAdmin };
