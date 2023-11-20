c657c24b18e37e669be866140b50d06e21ce3cb5
unknown <juthikahazra11@gmail.com>
2023-11-20 15:19:35 +0530

    admin router update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 //router
 app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
-app.use('/api/admin')
+app.use('/api/admin',adminRouter)
 
 
 app.listen(process.env.PORT || 5000)
