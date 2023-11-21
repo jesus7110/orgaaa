d52f59e32dbbe8e1b576a8f301b62ee154a817b4
unknown <juthikahazra11@gmail.com>
2023-11-21 11:22:19 +0530

    changes according to api testing

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -34,4 +34,35 @@ const authorizeAdmin = (req, res, next) => {
   next();
 };
 
-module.exports = { authenticateToken, authorizeAdmin };
+const authenticateUserToken = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization');
+
+    if (!token) {
+      return res.status(401).json({ success: false, message: 'Unauthorized: Token not provided' });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    // Check if the user exists
+    const user = await User.findById(decoded._id);
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    req.user = user; // Set the user information in the request object
+    next();
+  } catch (error) {
+    console.error('Error during user token authentication:', error.message);
+
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ success: false, message: 'Unauthorized: Token has expired' });
+    }
+
+    return res.status(403).json({ success: false, message: 'Forbidden: Invalid token' });
+  }
+};
+
+
+module.exports = { authenticateToken, authorizeAdmin, authenticateUserToken };
