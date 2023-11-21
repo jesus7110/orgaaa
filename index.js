0261969769785c17f218cecf4fa0c6336b424313
unknown <juthikahazra11@gmail.com>
2023-11-21 11:14:30 +0530

    routes imported and routed in index of the project

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/database");
 const userRouter = require('./routers/userRouter');
 const productRouter = require('./routers/productRouter');
 const adminRouter = require('./routers/adminRouter');
+const orderRouter = require('./routers/orderRouter');
 const app = express()
 
 app.use(express.urlencoded({extended: false }));
