6d14e67c52c9020689462e35d5d3f1973126a7ce
unknown <juthikahazra11@gmail.com>
2023-11-21 11:40:03 +0530

    user controller updated token genration and authorization

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,20 @@ const otpGenerator = require('otp-generator');
 const { User } = require('../models/userModel');
 const { Otp } = require('../models/otpModel');
 
+const jwt = require('jsonwebtoken');
+const generateAuthToken = (user) => {
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        number: user.number, // Use mobileNumber instead of username
+        // Add any other user information you want to include in the token
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: '2h' } // Token will expire in 2 hours
+    );
+  
+    return token;
+  };
 
 module.exports.signUp = async (req, res) => {
     const user = await User.findOne({
@@ -40,14 +54,20 @@ module.exports.verifyOtp = async (req, res) => {
 
     if (rightOtpFind.number === req.body.number && validUser) {
         const user = new User(_.pick(req.body, ["number"]));
-        const token = user.generateJWT();
+       // const token = user.generateJWT();
+
+        // Generate and send the JWT token in the response
+        const authToken = generateAuthToken(user);
+        
+        // to dev- debug -- delete it during production
+        console.log(authToken);
         const result = await user.save();
         const OTPDelete = await Otp.deleteMany({
             number: rightOtpFind.number
         });
         return res.status(200).send({
             message: "User Registration Successfull!",
-            token: token,
+            authToken,
             data: result
         });
     } else {
