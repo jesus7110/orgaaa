c2273eca2f571b363ad344ab16b6cc038c96a13e
unknown <juthikahazra11@gmail.com>
2023-11-21 11:07:27 +0530

    order router updated imp req model+ controller modified

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -3,3 +3,10 @@ const router = express.Router();
 const { placeOrder, updateOrderStatus } = require('./yourOrderController');
 const { authenticateToken, authorizeAdmin } = require('./yourAuthMiddleware'); // Include your authentication middleware
 
+// Route to place an order by a user
+router.post('/placeOrder', authenticateToken, placeOrder);
+
+// Route to update the status of an order by admin
+router.patch('/updateOrderStatus', authenticateToken, authorizeAdmin, updateOrderStatus);
+
+module.exports = router;
